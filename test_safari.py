9d78de0da043f95be4d6a96483ad0bf80b592382
Jessica Sadler <jessicagsadler@gmail.com>
2021-06-15 18:19:36 +0200

    changed assertions

--- test_safari.py
+++ test_safari.py
@@ -8,20 +8,20 @@ def test_feature_status_page_search(safari_driver):
     search_box = safari_driver.find_element_by_id("search")
     search_box.send_keys("CSS")
     value = search_box.get_attribute("value")
-    safari_driver.assertTrue(len(value) > 0)
+    assert len(value) > 0, "value is missing"
     search_box.submit()
     time.sleep(1)
     # Count the visible results when filters are applied
     # so one result shows up in at most one filter
     feature_count = safari_driver.shown_feature_count()
-    safari_driver.assertTrue(feature_count > 0)
+    assert feature_count > 0, "value is missing"
     
 def test_feature_status_page_filters(safari_driver):
     safari_driver.get("https://webkit.org/status/")
         
     time.sleep(1)
     filters = safari_driver.execute_script("return document.querySelectorAll('.filter-toggle')")
-    safari_driver.assertTrue(len(filters) == 7)
+    assert (len(filters) == 7), "value is not 7"
     
     # Make sure every filter is turned off.
     for checked_filter in filter(lambda f: f.is_selected(), filters):
@@ -30,7 +30,7 @@ def test_feature_status_page_filters(safari_driver):
     # Make sure you can select every filter.
     for filt in filters:
         filt.click()
-        safari_driver.assertTrue(filt.is_selected())
+        assert (filt.is_selected()), "filt is not selected"
         filt.click()
 
 def shown_feature_count(safari_driver):
