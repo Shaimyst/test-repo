8f89ca4ccaae8953e6c0dab08c44edd15944f10a
Jessica Sadler <jessicagsadler@gmail.com>
2021-07-01 20:02:54 +0200

    list exercises

--- test_lists.py
+++ test_lists.py
@@ -1,2 +1,7 @@
 # this is a document to work on list types and sorting gathered information
 
+mylist = ["apple", "banana", "cherry"]
+
+crochetlist = ["scarf", "hat", "gloves"]
+
+print(crochetlist)
