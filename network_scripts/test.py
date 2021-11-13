15cba978ccd51cd0134efffa61618c41a7294855
Jessica Sadler <jessicagsadler@gmail.com>
2021-11-13 16:13:11 +0100

    modified test doc to print types

--- network_scripts/test.py
+++ network_scripts/test.py
@@ -1,11 +1,16 @@
 # goal: make network requests with python script
 
 import requests
+import json
 
 # res = requests.get('https://example.com/')
 # print(res.text)
 
 
 res = requests.get('https://www.epidemicsound.com/api/subscription_service/recurly/plans/')
-print(type(res.text))
 
+res_json = res.text
+res_data = json.loads(res_json)
+back_to_json = json.dumps(res_data)
+print(type(str(res_data)))
+print(type(back_to_json))
