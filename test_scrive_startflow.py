883b78412d0762542c7d7d64e070f913bf1a8954
Jessica Sadler <jessicagsadler@gmail.com>
2022-10-04 14:19:31 +0200

    Update test_scrive_startflow.py

--- test_scrive_startflow.py
+++ test_scrive_startflow.py
@@ -6,3 +6,17 @@ from webdriver_manager import driver
 def test_signin_button(chrome_driver):
     chrome_driver.get("http://staging.scrive.com")
     time.sleep(10)
+
+# login: jessica.sadler+contacts@scrive.com
+# password: 'qqqqqq'
+# 
+# sign in or save credentials
+#
+# steps:
+# go to home page
+# start a flow with creator and 2nd signing party and 2 docs
+# start signing process
+# sign and complete flow
+# 
+# assert completion
+# 
